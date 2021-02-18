70d8d58bce187ca8ce37ac1fa144ee793d656042
Johanna <pera.johanna.maria@gmail.com>
2021-02-18 17:22:43 +0200

    committaan salasanavaihtoclassin alotuksen ja jotta saan toimivan version

---
+++ TuntiLaskin/TuntiLaskin/SalasananVaihto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuntiLaskin
+{
+    class SalasananVaihto
+    {
+        public string Salasana { get; set; }
+
+        public SalasananVaihto(string salasana)
+        {
+            this.Salasana = salasana;
+        }
+
+        public void SalasanaVaihdetaan(string SyotettySalasana)
+        {
+            Salasana = Console.ReadLine();
+            //SyotettySalasana 
+        }
+
+    }
+}
