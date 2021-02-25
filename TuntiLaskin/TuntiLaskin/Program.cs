d9e440b565086e7c0930b4ed067a0ec4d3e976cb
salonen99 <77480191+salonen99@users.noreply.github.com>
2021-02-25 18:52:46 +0200

    Update Program.cs

--- TuntiLaskin/TuntiLaskin/Program.cs
+++ TuntiLaskin/TuntiLaskin/Program.cs
@@ -7,6 +7,42 @@ using System.Threading.Tasks;
 
 namespace TuntiLaskin
 {
+        class SalasananVaihto
+    {
+        //konstruktori
+        public SalasananVaihto()
+        {
+        }
+
+        /*luokan tärkein metodi. Kun metodia kutsutaan pääohjelmassa,  syötetään samalla nykyinen salasana.*/
+        public string SalasanaVaihdetaan()
+        {
+            while(true)
+            {
+                //pyydetää uus salasana ja tallennetaan UusiSalasana muuttujaan
+                Console.WriteLine("Anna uusi salasana.");
+                string UusiSalasana = Console.ReadLine();
+
+                Console.WriteLine("Anna uusi salasana uudelleen.");
+                string UusiSalasanaToistamiseen = Console.ReadLine();
+
+                if(UusiSalasana == UusiSalasanaToistamiseen)
+                {
+                    //Syötetty salasana palautetaan vahvistuksen jälkeen
+                    Console.WriteLine("Salasana on muutettu");
+                    Console.WriteLine();
+                    return UusiSalasana;
+                }
+                else
+                {
+                    Console.WriteLine("Syötä salasana uudelleen.");
+                    Console.WriteLine("--------------------------");
+                }
+
+            }
+        }
+
+    }
     class Program
     {
        public static string SalasananSuojaus()
